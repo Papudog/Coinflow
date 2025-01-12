b5eb76ed5f5939745664f88ae0addb71d9f4851d
Roberto Amador <josarroliga0904@gmail.com>
2025-01-12 00:26:36 -0600

    fix(pie_graph): Issues with mixed transactions types resolved

--- src/components/transactions/pie_graph.tsx
+++ src/components/transactions/pie_graph.tsx
@@ -1,42 +1,63 @@
 import { StyleSheet, Text, View } from "react-native";
 import { theme } from "@/src/constants/theme";
 import { PieChart } from "react-native-gifted-charts";
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { useTransaction } from "@/src/context/transaction_context";
 import { Transaction } from "@/src/models/transactions";
 
-type CategoryReduce = { [key: number]: number };
 interface PieData {
   value: number;
   name: string | undefined;
   color: string | undefined;
   type: string | undefined;
-  focused: boolean;
 }
 
 export default function PieGraph(): React.JSX.Element {
   const { transactions, typeSwitch } = useTransaction();
   const [selectedValue, setSelectedValue] = useState<PieData | null>(null);
 
-  const addCategoryAmount = transactions.reduce(
-    (acc: CategoryReduce, transaction: Transaction) => {
-      const { category_id, amount } = transaction
-      acc[category_id] = 0;
+  const addCategoryAmount: Record<string, number> =
+    useMemo(() => {
+      return transactions
+        .filter((transaction: Transaction) => transaction.type === typeSwitch)
+        .reduce((acc: { [key: string]: number }, transaction: Transaction) => {
+          const { category_id, amount, type } = transaction
+          const catTypeKey: string = `${category_id}-${type}`;
 
-      acc[category_id] += amount;
-      return acc;
-    }, {});
+          if (!acc[catTypeKey]) acc[catTypeKey] = 0;
 
-  const pieData: PieData[] = transactions
-    .map((transaction: Transaction) => ({
-      value: addCategoryAmount[transaction.category_id!],
-      name: transaction.categories?.name,
-      color: transaction.categories?.color,
-      type: transaction.type,
-      focused: true
-    }))
-    .filter((transaction) => transaction.type === typeSwitch)
-    .sort((a, b) => b.value - a.value);
+          acc[catTypeKey] += amount;
+          return acc;
+        }, {}); // {"1-Expense": 40, "1-Income": 10}
+    }, [transactions, typeSwitch])
+
+  const pieData: PieData[] = useMemo(() => {
+    const uniqueCategories: Set<string> = new Set<string>();
+
+    return transactions
+      .filter((transaction: Transaction) => {
+        const { category_id, type } = transaction
+        const catTypeKey: string = `${category_id}-${type}`;
+
+        if (uniqueCategories.has(catTypeKey)) return false;
+
+        uniqueCategories.add(catTypeKey);
+        return true;
+      })
+      .map((transaction: Transaction) => {
+        const { category_id, type } = transaction;
+        const catTypeKey: string = `${category_id}-${type}`;
+
+        return {
+          value: addCategoryAmount[catTypeKey],
+          name: transaction.categories?.name,
+          color: transaction.categories?.color,
+          type: transaction.type,
+        }
+      })
+      .filter((transaction) => transaction.type === typeSwitch)
+      .sort((a, b) => b.value - a.value);
+  }, [transactions, typeSwitch, addCategoryAmount])
 
   useEffect(() => {
     setSelectedValue(null)
@@ -57,11 +78,11 @@ export default function PieGraph(): React.JSX.Element {
         innerCircleColor={theme.dark}
         centerLabelComponent={(): React.JSX.Element => (
           <View style={{ alignItems: "center", justifyContent: "center" }}>
-            <Text style={{ ...styles.text, fontSize: 10 }}>{pieData[0].name}</Text>
-            <Text style={{ ...styles.text, fontSize: 18 }}>{`$ ${pieData[0].value}`}</Text>
+            <Text style={{ ...styles.text, fontSize: 10 }}>{selectedValue?.name ?? '='}</Text>
+            <Text style={{ ...styles.text, fontSize: 18 }}>{`$ ${selectedValue?.value ?? ''}`}</Text>
           </View>
         )}
-      // onPress={(data: PieData) => setSelectedValue(data)}
+        onPress={(data: PieData) => setSelectedValue(data)}
       />
     </View>
   );
